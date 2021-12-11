45f0ab94eb5e59861a8b498e06b44c5e22044900
Marcos Paulo <marcos@meucredere.com.br>
2021-12-10 21:54:04 -0300

    manually erlang nodes communication

---
+++ lib/clustlixir.ex
@@ -0,0 +1,18 @@
+defmodule Clustlixir do
+  @moduledoc """
+  Documentation for `Clustlixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Clustlixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/clustlixir_test.exs
@@ -0,0 +1,8 @@
+defmodule ClustlixirTest do
+  use ExUnit.Case
+  doctest Clustlixir
+
+  test "greets the world" do
+    assert Clustlixir.hello() == :world
+  end
+end
