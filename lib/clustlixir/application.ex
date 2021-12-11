c5ee0962b544427976827285578cfa30a4ce53f8
Marcos Paulo <marcos@meucredere.com.br>
2021-12-10 22:04:59 -0300

    add libcluster supervisor

--- lib/clustlixir/application.ex
+++ lib/clustlixir/application.ex
@@ -8,6 +8,7 @@ defmodule Clustlixir.Application do
   @impl true
   def start(_type, _args) do
     children = [
+      {Cluster.Supervisor, [topologies(), [name: Clustlixir.ClusterSupervisor]]},
       # Starts a worker by calling: Clustlixir.Worker.start_link(arg)
       # {Clustlixir.Worker, arg}
       Clustlixir.Observer,
@@ -18,5 +19,19 @@ defmodule Clustlixir.Application do
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Clustlixir.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  defp topologies do
+    [
+      example: [
+        strategy: Cluster.Strategy.Epmd,
+        config: [
+          hosts: [
+            :"n1@127.0.0.1",
+            :"n2@127.0.0.1"
+          ]
+        ]
+      ]
+    ]
   end
 end
